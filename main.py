1c6fbc761ab990f2b73ee7465eed689d0453d703
asif-bappy <asifikbal0033@gmail.com>
2025-04-12 01:59:56 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# Variable & Data type
+name= 'asif'
+age= 27
+student= False
+
+print(type(name))
+print(type(age))
+print(type(student))
+
+# Aritmetic Operators
+print(age*2)
+print(age+2)
+print(age/2)
+print(age-2)
+# Comparision Operators
+print(age>=18)
+print(age==18)
+print(age<=18)
+print(age!=18)
+# Logical Operators
+temp= 28
+sun= True
+if temp>=25 and sun== True:
+    print('Beautiful sunny day')
+else:
+    print('mao tempo')
+print()
+# Assignment Operators)
+num = 5
+num+=1
+print(num)
+num-=1
+print(num)
+num*=1
+print(num)
+num/=1
+print(num)
+
+# Identity Operators
+x= 5
+y= 6
+
+print(x==y)
+print(x!=y)
+
+# Membership Operators
+num = [1,2,3,4,5,6,7,8,9]
+print(0 in num)
+print(11 not in num)
